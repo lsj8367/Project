c09e434ffdf2fd96a4829902ff31766c3bc1d71c
lsj8367 <lsj8367@naver.com>
2021-08-25 21:45:29 +0900

    refactor: service 로직 분리

--- team3project/src/main/java/pack/user/controller/BuyMainController.java
+++ team3project/src/main/java/pack/user/controller/BuyMainController.java
@@ -4,37 +4,18 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
-import pack.model.NewBookDto;
-import pack.user.model.NewBookListDao;
-
-import java.util.List;
+import pack.user.service.BuyMainService;
 
 @Controller
 @RequiredArgsConstructor
 public class BuyMainController {
 
-	private final NewBookListDao newBookListDao;
+	private final BuyMainService buyMainService;
 
 	@RequestMapping("buymain")
 	public ModelAndView main() {
-		ModelAndView modelAndView = new ModelAndView();
-		
-		//베스트셀
-		NewBookDto bestseller = newBookListDao.selectBest();
-		modelAndView.addObject("bestseller", bestseller);
-		
-		
-		//조회수top3
-		List<NewBookDto> readTop3List = newBookListDao.selectReadTop3();
-		modelAndView.addObject("readtop3", readTop3List);
-		
-		//랜덤10권		
-		List<NewBookDto> randam10List = newBookListDao.selectRandom10();
-		modelAndView.addObject("random10", randam10List);
-
-
+		ModelAndView modelAndView = buyMainService.buyMain();
 		modelAndView.setViewName("buymain");
-		
 		return modelAndView;
 	}
 }
