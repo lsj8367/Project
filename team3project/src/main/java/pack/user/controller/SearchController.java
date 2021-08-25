9e046a39901e3758dd80b718c00dc845df0f9046
lsj8367 <lsj8367@naver.com>
2021-08-25 22:27:52 +0900

    refactor: jdbc -> Mybatis 변경

--- team3project/src/main/java/pack/user/controller/SearchController.java
+++ team3project/src/main/java/pack/user/controller/SearchController.java
@@ -2,31 +2,27 @@ package pack.user.controller;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 import pack.model.NewBookDto;
-import pack.model.SearchInter;
+import pack.model.SearchDao;
 
 import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 @RequiredArgsConstructor
 public class SearchController {
-	private final SearchInter searchInter;
+	private final SearchDao searchDao;
 	
 	@RequestMapping("search")
-	public ModelAndView DataAll(HttpServletRequest request, HttpServletResponse response, Model model) {
+	public ModelAndView DataAll(HttpServletRequest request) {
 		ModelAndView modelAndView = new ModelAndView();
-		
-		String type = request.getParameter("type");
 		String search = request.getParameter("search");
 		
-		ArrayList<NewBookDto> list = searchInter.getDataAll(type, search);
-		modelAndView.setViewName("list");
+		List<NewBookDto> list = searchDao.getDataAll(search);
 		modelAndView.addObject("newbooklist", list);
+		modelAndView.setViewName("list");
 		
 		return modelAndView;
 	}
