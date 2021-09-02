10ed48a063c4db829830465fade1c99fc689691a
lsj8367 <lsj8367@naver.com>
2021-09-02 23:15:23 +0900

    refactor: import 통합

--- team3project/src/main/java/pack/repository/AdminRepository.java
+++ team3project/src/main/java/pack/repository/AdminRepository.java
@@ -1,11 +1,12 @@
 package pack.repository;
 
-import java.util.List;
-import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import pack.domain.entity.Admin;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface AdminRepository extends JpaRepository<Admin, Long> {
     Optional<Admin> findByAdminId(String adminId);
-    List<Admin> findAllByAdminAcc(String adminAcc); //0 고정
+    List<Admin> findAllByAdminAcc(int adminAcc); //0 고정
 }
