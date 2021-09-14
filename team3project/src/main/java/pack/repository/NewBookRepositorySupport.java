084168342b39c3fba1a343ceb0269a860c32f7b0
lsj8367 <lsj8367@naver.com>
2021-09-14 23:41:55 +0900

    feat: querydsl, 엔티티 빌더 생성

--- team3project/src/main/java/pack/repository/NewBookRepositorySupport.java
+++ team3project/src/main/java/pack/repository/NewBookRepositorySupport.java
@@ -1,7 +1,10 @@
 package pack.repository;
 
+import com.querydsl.core.Tuple;
 import pack.domain.entity.NewBook;
 
 public interface NewBookRepositorySupport {
     NewBook selectBestSeller();
+    Tuple getMostSellBook();
+    void upNbstock(int plusStock, Long id);
 }
--- team3project/src/main/java/pack/repository/NewBookRepositorySupportImpl.java
+++ team3project/src/main/java/pack/repository/NewBookRepositorySupportImpl.java
@@ -1,5 +1,6 @@
 package pack.repository;
 
+import com.querydsl.core.Tuple;
 import com.querydsl.jpa.JPAExpressions;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import lombok.RequiredArgsConstructor;
@@ -18,5 +19,23 @@ public class NewBookRepositorySupportImpl implements NewBookRepositorySupport {
                         .eq(JPAExpressions.select(newBook.nbScount.max())
                                 .from(newBook)))
                 .fetchOne();
+    }
+
+    @Override
+    public Tuple getMostSellBook() {
+        return jpaQueryFactory.select(newBook.id, newBook.nbName, newBook.nbAuthor,
+                newBook.nbComp, newBook.nbScount)
+                .from(newBook)
+                .where(newBook.nbScount
+                        .eq(JPAExpressions.select(newBook.nbScount.max())
+                                .from(newBook)))
+                .fetchOne();
+    }
+
+    @Override
+    public void upNbstock(final int plusStock, final Long id) {
+        jpaQueryFactory.update(newBook)
+                .set(newBook.nbStock, newBook.nbStock.add(plusStock))
+                .where(newBook.id.eq(id));
     }
 }
