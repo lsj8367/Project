f78f5b279fdab7e1ae876e7ef0c38764a35589b0
SeungJae Lee <lsj8367@naver.com>
2021-09-15 16:49:32 +0900

    test: 테스트 케이스 추가

--- team3project/src/test/java/pack/repository/FaqBoardRepositoryTest.java
+++ team3project/src/test/java/pack/repository/FaqBoardRepositoryTest.java
@@ -1,17 +1,17 @@
 package pack.repository;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.List;
+import javax.transaction.Transactional;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 import org.springframework.context.annotation.Import;
 import pack.config.QuerydslConfig;
 import pack.domain.entity.FaqBoard;
-
-import javax.transaction.Transactional;
-import java.util.List;
-
-import static org.assertj.core.api.Assertions.assertThat;
 
 @DataJpaTest
 @Import(QuerydslConfig.class)
@@ -19,14 +19,17 @@ class FaqBoardRepositoryTest {
     @Autowired
     FaqBoardRepository faqBoardRepository;
 
+    @Autowired
+    TestEntityManager testEntityManager;
+
     @Test
-    @DisplayName("selectFaqAll, selectQnaAll")
+    @DisplayName("selectFaqAll")
     void selectFaqAll() {
         faqBoardRepository.findAll();
     }
 
     @Test
-    @DisplayName("selectfaqType, selectQnaClass")
+    @DisplayName("selectQnaClass")
     void selectfaqType() {
         List<FaqBoard> faqBoardList = faqBoardRepository.findAllByFaqType("주문");
         assertThat(faqBoardList.size()).isEqualTo(0);
@@ -44,5 +47,20 @@ class FaqBoardRepositoryTest {
         faqBoardRepository.save(faqBoard);
 
         assertThat(faqBoard.getFaqNo()).isEqualTo(1L);
+    }
+
+    @Test
+    void selectFaqDetail() {
+//    select * from faqboard where faq_no =#{faq_no}
+        FaqBoard faqBoard = FaqBoard.builder()
+            .faqTitle("테스트")
+            .faqContent("테스트")
+            .faqType("주문")
+            .build();
+
+        faqBoardRepository.save(faqBoard);
+        testEntityManager.clear();
+
+        faqBoardRepository.findById(1L);
     }
 }
