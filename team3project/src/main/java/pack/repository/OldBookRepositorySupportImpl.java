56ef275f9c82a65339e92c00d320d5cafa300d44
lsj8367 <lsj8367@naver.com>
2021-09-21 19:13:07 +0900

    refactor: oldGenre2 까지 완료

---
+++ team3project/src/main/java/pack/repository/OldBookRepositorySupportImpl.java
@@ -0,0 +1,44 @@
+package pack.repository;
+
+import static pack.domain.entity.QOldBook.oldBook;
+import com.querydsl.jpa.JPAExpressions;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import pack.domain.entity.OldBook;
+
+@RequiredArgsConstructor
+public class OldBookRepositorySupportImpl implements OldBookRepositorySupport {
+    private static final String FIRST_GRADE = "1";
+    private static final String SECOND_GRADE = "2";
+    private static final String THIRD_GRADE = "3";
+
+
+    private final JPAQueryFactory jpaQueryFactory;
+
+    @Override
+    public OldBook getMostRentBook() {
+        return jpaQueryFactory.selectFrom(oldBook)
+            .where(oldBook.obScount.eq(JPAExpressions.select(oldBook.obScount.max())
+                .from(oldBook)))
+            .fetchOne();
+    }
+
+    @Override
+    public List<OldBook> genreForFirstGrade(String obGenre) {
+        return jpaQueryFactory.selectFrom(oldBook)
+            .where(oldBook.obState.eq(FIRST_GRADE).and(oldBook.obDonor.ne("10"))
+                .and(oldBook.obGenre.contains(obGenre)))
+            .fetch();
+    }
+
+    @Override
+    public List<OldBook> genreForAnotherGrade(String obGenre) {
+        return jpaQueryFactory.selectFrom(oldBook)
+            .where(oldBook.obGenre.contains(obGenre)
+                .and(oldBook.obState.eq("2"))
+                .or(oldBook.obState.eq("3")))
+            .fetch();
+    }
+
+}
