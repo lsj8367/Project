c1649ea9b4e2a67282d606015e409bb930743c3a
lsj8367 <lsj8367@naver.com>
2021-09-30 21:28:09 +0900

    refactor: modelAndView 체이닝 작성

--- team3project/src/main/java/pack/mypage/controller/MydonorController.java
+++ team3project/src/main/java/pack/mypage/controller/MydonorController.java
@@ -1,29 +1,25 @@
 package pack.mypage.controller;
 
+import java.util.Map;
+import javax.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
-import pack.model.OldBookDto;
 import pack.mypage.model.MydonorImpl;
-
-import javax.servlet.http.HttpSession;
-import java.util.List;
 
 @Controller
 @RequiredArgsConstructor
 public class MydonorController {
-	private final MydonorImpl mydonorImpl;
 
-	@RequestMapping("mydonor")
-	public ModelAndView mydonorlist(HttpSession session) {
-		ModelAndView modelAndView=new ModelAndView();
-		String id = (String)session.getAttribute("id");
-		//도서 기부 내역 모델앤뷰
-		List<OldBookDto> donorlistall = mydonorImpl.donorListbyId(id);
-		modelAndView.setViewName("mypage/mydonor");
-		modelAndView.addObject("dnabook", donorlistall);
-		
-		return modelAndView;
-	}
+    private final MydonorImpl mydonorImpl;
+
+    @RequestMapping("mydonor")
+    public ModelAndView mydonorlist(HttpSession session) {
+        String id = (String) session.getAttribute("id");
+        return new ModelAndView("mypage/mydonor", Map.of(
+            "dnabook", mydonorImpl.donorListbyId(id)
+        ));
+    }
+
 }
--- team3project/src/main/java/pack/mypage/service/MypageService.java
+++ team3project/src/main/java/pack/mypage/service/MypageService.java
@@ -1,18 +1,14 @@
 package pack.mypage.service;
 
-import com.querydsl.core.Tuple;
-import java.util.List;
+import java.util.Map;
 import javax.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.web.servlet.ModelAndView;
-import pack.domain.entity.NewBook;
-import pack.model.InqueryDto;
-import pack.model.OldBookDto;
-import pack.model.OrderInfoDto;
 import pack.mypage.model.MypageImpl;
 import pack.repository.CardInfoRepository;
 import pack.repository.NewBookRepository;
+import pack.repository.OldBookRepository;
 
 @Service
 @RequiredArgsConstructor
@@ -20,37 +16,18 @@ import pack.repository.NewBookRepository;
 public class MypageService {
     private final CardInfoRepository cardInfoRepository;
     private final NewBookRepository newBookRepository;
+    private final OldBookRepository oldBookRepository;
     private final MypageImpl mypageImpl;
 
     public ModelAndView list3(String user_id) {
-        ModelAndView modelAndView = new ModelAndView();
-
-        //최근 주문 내역 3 모델앤뷰
-        List<OrderInfoDto> orderlist = mypageImpl.orderlist(user_id);
-
-        //최근 대여 내역 3 모델앤뷰
-        List<OldBookDto> rentlist = mypageImpl.rentlist(user_id);
-
-        //최근 도서 기부 내역 3 모델앤뷰
-        List<OldBookDto> donorlist = mypageImpl.donorlist(user_id);
-
-        //랜덤 새책 추천
-        NewBook randNewbook = newBookRepository.recommandNewBook();
-
-        //최근 카드정보 내역 3 모델앤뷰
-        List<Tuple> cardList = cardInfoRepository.cardList(user_id);
-
-        //최근 문의 내역 3 모델앤뷰
-        List<InqueryDto> inqlist = mypageImpl.inqlist(user_id);
-
-        modelAndView.addObject("odbook", orderlist);
-        modelAndView.addObject("rtbook", rentlist);
-        modelAndView.addObject("dnbook", donorlist);
-        modelAndView.addObject("randNewbook", randNewbook);
-        modelAndView.addObject("cdinfo", cardList);
-        modelAndView.addObject("inqinfo", inqlist);
-
-        return modelAndView;
+        return new ModelAndView("mypage", Map.of(
+            "odbook", mypageImpl.orderlist(user_id),
+            "rtbook", mypageImpl.rentlist(user_id),
+            "dnbook", oldBookRepository.findTop3ByObDonorOrderByObNameDesc(user_id),
+            "randNewbook", newBookRepository.recommandNewBook(),
+            "cdinfo", cardInfoRepository.cardList(user_id),
+            "inqinfo", mypageImpl.inqlist(user_id)
+        ));
     }
 
 }
