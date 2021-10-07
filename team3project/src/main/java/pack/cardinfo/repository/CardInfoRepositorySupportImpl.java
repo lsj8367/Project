e5c54e7fe7442e204a16ec7d12606a4645ede11e
lsj8367 <lsj8367@naver.com>
2021-10-07 22:20:22 +0900

    refactor: tuple객체 엔티티로 변경

--- team3project/src/main/java/pack/cardinfo/repository/CardInfoRepositorySupportImpl.java
+++ team3project/src/main/java/pack/cardinfo/repository/CardInfoRepositorySupportImpl.java
@@ -3,8 +3,10 @@ package pack.cardinfo.repository;
 import static pack.cardinfo.domain.QCardInfo.cardInfo;
 import com.querydsl.core.Tuple;
 import com.querydsl.jpa.impl.JPAQueryFactory;
+import java.util.ArrayList;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
+import pack.cardinfo.domain.CardInfo;
 
 @RequiredArgsConstructor
 public class CardInfoRepositorySupportImpl implements CardInfoRepositorySupport {
@@ -12,11 +14,21 @@ public class CardInfoRepositorySupportImpl implements CardInfoRepositorySupport 
     private final JPAQueryFactory jpaQueryFactory;
 
     @Override
-    public List<Tuple> cardList(String cardOwnerId) {
-        return jpaQueryFactory.select(cardInfo.cardNo, cardInfo.cardComp).from(cardInfo)
+    public List<CardInfo> cardList(String cardOwnerId) {
+        final List<Tuple> tupleList = jpaQueryFactory.select(cardInfo.cardNo, cardInfo.cardComp).from(cardInfo)
             .where(cardInfo.cardOwnerid.eq(cardOwnerId))
             .orderBy(cardInfo.cardComp.desc())
             .fetch();
+
+        List<CardInfo> cardInfoList = new ArrayList<>();
+        for (Tuple tuple : tupleList) {
+            cardInfoList.add(CardInfo.builder()
+                .cardNo(tuple.get(cardInfo.cardNo))
+                .cardComp(tuple.get(cardInfo.cardComp))
+                .build());
+        }
+        return cardInfoList;
+
     }
 
 }
