bf3c6540c2d92973cac832d915a70fad5bcec352
lsj8367 <lsj8367@naver.com>
2021-10-31 16:53:37 +0900

    feat: rentinfo JPA init

---
+++ team3project/src/main/java/pack/rentinfo/repository/RentinfoRepositorySupport.java
@@ -0,0 +1,8 @@
+package pack.rentinfo.repository;
+
+import java.util.List;
+import pack.rentinfo.domain.Rentinfo;
+
+public interface RentinfoRepositorySupport {
+    List<Rentinfo> findAllByRentBook();
+}
